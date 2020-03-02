9f241c0391f36f14385ef32a81818242e0cf426d
Davidama <davidarteromartinez@gmail.com>
2020-03-02 18:44:38 +0100

    Accepted

---
+++ DondeEstaLaBolita.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class DondeEstaLaBolita {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int posact,quant,pos1,pos2;
+
+        quant=sc.nextInt();
+        posact=sc.nextInt();
+        while(quant!=0 && posact!=0){
+            pos1=1;
+            pos2=1;
+
+                while(pos1!=0 && pos2!=0){
+                pos1=sc.nextInt();
+                pos2=sc.nextInt();
+
+                if(posact==pos1){
+                    posact=pos2;
+                }
+                else if(posact==pos2){
+                        posact=pos1;
+                    }
+            }
+            System.out.println(posact);
+            quant=sc.nextInt();
+            posact=sc.nextInt();
+        }
+    }
+}
